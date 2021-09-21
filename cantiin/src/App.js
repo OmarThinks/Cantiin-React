9c5ecbb9abdceaa35f17afd2f8ca169fe8a31478
Omar Magdy <contact.parmag@gmail.com>
2021-09-21 16:03:47 +0200

    home page

--- cantiin/src/App.js
+++ cantiin/src/App.js
@@ -3,7 +3,52 @@ import './App.css';
 function App() {
   return (
 
-    <div>Welcome Home</div>
+
+
+
+
+
+
+    <div>
+    <ul>
+      <li>
+        <a href="https://cantiin.com/">Home</a>
+      </li>
+      <li>
+        <a href="https://cantiin.com/products/">
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="https://cantiin.com/api/">
+          API Auth
+        </a>
+      </li>
+
+
+      <li>
+        <a href="https://cantiin.com/api/">
+          Login
+        </a>
+      </li>
+      <li>
+        <a href="https://cantiin.com/api/">
+          Sign Up
+        </a>
+      </li>
+      <li>Login</li>
+      <li>Sign Up</li>
+    </ul>
+
+
+
+    <h1>  
+      Welcome Home
+    </h1>
+      
+    </div>
+
+
 
   );
 }
