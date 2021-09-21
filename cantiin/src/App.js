615f99af1468afff3f9a92e59e85e446a3206ad6
Omar Magdy <contact.parmag@gmail.com>
2021-09-21 16:32:47 +0200

    navbar works as a component

--- cantiin/src/App.js
+++ cantiin/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route} from 'react-router-dom'
 
 import {backend_urls,frontend_urls} from './settings/urls';
 
-
+import NavBar from "./components/navbar";
 
 
 
@@ -16,37 +16,9 @@ function App() {
 
 
 
-
     <div>
-    <ul>
-      <li>
-        <a href={frontend_urls.home}>Home</a>
-      </li>
-      <li>
-        <a href={frontend_urls.products.list}>
-          Products
-        </a>
-      </li>
+     <NavBar isLoggedIn={true} />
 
-
-
-      <li>
-        <a href={frontend_urls.auth.login}>
-          Login
-        </a>
-      </li>
-      <li>
-        <a href={frontend_urls.auth.signup}>
-          Sign Up
-        </a>
-      </li>
-      <li>
-        <a href={frontend_urls.auth.logout}>
-          Log Out
-        </a>
-      </li>
-
-    </ul>
 
 
 
--- cantiin/src/components/navbar.js
+++ cantiin/src/components/navbar.js
@@ -1,7 +1,43 @@
-class NavBar extends React.Component {
-    render() { 
-        return <div></div>;
-    }
+import {backend_urls,frontend_urls} from '../settings/urls';
+
+
+
+
+const NavBar = (isLoggedIn) => {
+    
+    return(
+        <div>
+               <ul>
+                    <li>
+                        <a href={frontend_urls.home}>Home</a>
+                    </li>
+                    <li>
+                        <a href={frontend_urls.products.list}>
+                        Products
+                        </a>
+                    </li>
+
+
+
+                    <li>
+                        <a href={frontend_urls.auth.login}>
+                        Login
+                        </a>
+                    </li>
+                    <li>
+                        <a href={frontend_urls.auth.signup}>
+                        Sign Up
+                        </a>
+                    </li>
+                    <li>
+                        <a href={frontend_urls.auth.logout}>
+                        Log Out
+                        </a>
+                    </li>
+
+                </ul>
+        </div>
+    )
 }
- 
+
 export default NavBar;
