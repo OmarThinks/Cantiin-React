9622ee608dc20daac77fa6b8fdcea62c2d99e1c0
Omar Magdy <contact.parmag@gmail.com>
2021-09-24 17:30:50 +0200

    urls.js fully separated

--- cantiin/src/functions/fetching/list.js
+++ cantiin/src/functions/fetching/list.js
@@ -1,5 +1,6 @@
 import {settings} from "../../settings";
-const qs = require('query-string');
+import {buildUrl, getPureUrl, getUrlQueryParameters,getUrlSpecificQueryPramater,
+    getUrlPage} from "../urls";
 
 
 function getItemsList(response)
@@ -10,76 +11,7 @@ function getAllResultsNumber(response)
 
 
 
-function buildUrl(pureUrl, params)
-{
-    //pureUrl="www.example.com";params={}; //www.example.com
-    //pureUrl="www.example.com";params={page:1}; //www.example.com?page=1
 
-    let paramsString =qs.stringify(params);
-    if(paramsString)
-    {return pureUrl+"?"+paramsString}
-    return pureUrl;
-}
-
-
-function getPureUrl(url)
-{
-    //url="www.example.com";// Just for testing without "?" 
-    //url="www.example.com?";// Just for testing with "?" empty 
-    //url="www.example.com?notpage=545";// Just for testing with "?" without page 
-    //url="www.example.com?page=545";// Just for testing with "?" without page
-    // all equal "www.example.com"
-    return url.split("?")[0];
-}
-
-
-
-
-
-
-function getUrlQueryParameters(url)
-{
-    //url="www.example.com";// Just for testing without "?" = 1
-    //url="www.example.com?";// Just for testing with "?" empty = 1
-    //url="www.example.com?notpage=545";// Just for testing with "?" without page =1 
-    //url="www.example.com?page=2";// Just for testing with "?" without page=2
-    let params = url.split("?")[1];
-    if(params){return qs.parse(params);}
-    else{return {};}
-}
-
-
-
-function getUrlSpecificQueryPramater(url, param,parser,defaultValue)
-{
-    let queryParam = (function(url, param,parser) {
-        try {
-          return parser(getUrlQueryParameters(url)[param]);
-        } catch (error) {
-            return null;
-        }
-      })(url, param,parser);
-
-    if(queryParam){return queryParam;}
-    return defaultValue;
-}
-
-
-
-
-
-function getUrlPage(url)
-{
-    //console.log(getUrlQueryParameters(url));
-    //url="www.example.com";// Just for testing without "?" = 1
-    //url="www.example.com?";// Just for testing with "?" empty = 1
-    //url="www.example.com?notpage=545";// Just for testing with "?" without page =1 
-    //url="www.example.com?page=2";// Just for testing with "?" without page=2
-
-    //console.log(getUrlSpecificQueryPramater(url, "page",parseInt,1));
-    return getUrlSpecificQueryPramater(url, "page",parseInt,1);
-
-}
 
 function getCurrentWindowPage()
 {return getUrlPage(window.location.href);}
