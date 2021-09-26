d7f64195adc04ffb61642117da6aaefca56284df
Omar Magdy <contact.parmag@gmail.com>
2021-09-26 13:14:51 +0200

    navbar index button

--- cantiin/src/components/navbar.js
+++ cantiin/src/components/navbar.js
@@ -1,5 +1,34 @@
 import {settings} from '../settings';
-import { Fragment } from 'react';
+import { Fragment, Component } from 'react';
+
+
+class NavBarLi extends Component {
+    render() {
+
+        let {text} = this.props;
+        let {link}=this.props;
+
+
+/*
+
+            <button className="navBarIndexButton">
+                {text}
+            </button>
+
+*/
+
+        return <li className="navBarList-Index">
+        <a className="navBarList-Index-Link" link={`${link}`}>
+            <button className="navBarIndexButton">
+                {text}
+            </button>
+        </a>
+    </li>;
+    }
+}
+ 
+
+
 
 
 const NavBar = (props) => {
@@ -9,43 +38,48 @@ const NavBar = (props) => {
     let login_partial = "";
     if(isLoggedIn)
     {
-        console.log("He is Logged In");
-        login_partial=
-    <li className="navBarList-Index">
-        <a className="navBarList-Index-Link" href={settings.frontend_urls.auth.logout}>
-            Log Out
-        </a>
-    </li>;
+        //console.log("He is Logged In");
+        login_partial= <NavBarLi text="Log Out" link={settings.frontend_urls.auth.logout}/>;
+
     }
-    else{
-        console.log("He is Logged Out");
-        login_partial= <Fragment>
-        <li className="navBarList-Index">
-        <a className="navBarList-Index-Link" href={settings.frontend_urls.auth.login}>
-        Login
-        </a>
-    </li>
+
+/*
+
     <li className="navBarList-Index">
         <a className="navBarList-Index-Link" href={settings.frontend_urls.auth.signup}>
         Sign Up
         </a>
     </li>
+*/
+    else{
+        //console.log("He is Logged Out");
+        login_partial= <Fragment>
+        <NavBarLi text="Login" link={settings.frontend_urls.auth.login}/>
+        <NavBarLi text="Sign Up" link={settings.frontend_urls.auth.signup}/>
+
     </Fragment>
     ;
         
     }
 
-    return(
-        <div className="navBarDiv">
-               <ul className="navBarList">
+/*
+
                     <li className="navBarList-Index">
-                        <a className="navBarList-Index-Link" href={settings.frontend_urls.home}>Home</a>
+                        <a className="navBarList-Index-Link" href={settings.frontend_urls.products.list}>Home</a>
                     </li>
                     <li className="navBarList-Index">
                         <a className="navBarList-Index-Link" href={settings.frontend_urls.products.list}>
                         Products
                         </a>
                     </li>
+
+*/
+
+    return(
+        <div className="navBarDiv">
+               <ul className="navBarList">
+                    <NavBarLi text="Home" link={settings.frontend_urls.home}/>
+                    <NavBarLi text="Products" link={settings.frontend_urls.home}/>
                     </ul>
                 <ul className="navBarList navBarList-Right">
 
