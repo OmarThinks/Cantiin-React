f7ab84ef8a698bf4be750b1025beb53f17ca3088
Omar Magdy <contact.parmag@gmail.com>
2021-09-26 15:55:00 +0200

    styled instock and out of stock text

--- cantiin/src/components/cards/product.js
+++ cantiin/src/components/cards/product.js
@@ -11,15 +11,16 @@ class ProductCard extends Component {
     render() {
         let productCardClassName= "productCard productCardOutOfStock";
         let inStockText = "Out Of Stock"; 
+        let inStockStyle = "productCard-productInStock productCard-productInStock-Out";
         if(this.state.product.in_stock){
             productCardClassName= "productCard productCardInStock";
-            inStockText = "In Stock"; 
-
+            inStockText = "In Stock";
+            inStockStyle = "productCard-productInStock productCard-productInStock-In";
         }
         return <li className="productCardContainer">
             <div className={productCardClassName}>
                 <div className="productCard-productName">{this.state.product.name}</div>
-                <div className="productCard-productInStock">{inStockText}</div>
+                <div className={inStockStyle}>{inStockText}</div>
                 <div className="productCard-productPrice">${this.state.product.price}</div>
             </div>
         </li>;
