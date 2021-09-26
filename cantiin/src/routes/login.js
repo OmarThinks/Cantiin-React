2b12fe7ca6b92fe603ef2181be962537662d9084
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 00:51:44 +0200

    login Form added with material UI

--- cantiin/src/routes/login.js
+++ cantiin/src/routes/login.js
@@ -1,11 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
+
 const LoginRoute = (props) => {
-     
-        return <div>
+    
+    const classes = useStyles();
+    
+    return <div>
 
     <h1>  
       Login:
     </h1>
     
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Standard" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+
+
         </div>;
     }
  
