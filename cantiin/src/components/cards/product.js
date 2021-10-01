ef0cea886ef667ec0133bc5f52f7021ecadffa1f
Omar Magdy <contact.parmag@gmail.com>
2021-10-02 01:19:25 +0200

    removed comments of old components

--- cantiin/src/components/cards/product.js
+++ cantiin/src/components/cards/product.js
@@ -1,33 +1,6 @@
 import React, { Component, useState } from 'react';
 
 
-/*
-
-class ProductCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { "product":props.item }
-    }
-    render() {
-        let productCardClassName= "productCard productCardOutOfStock";
-        let inStockText = "Out Of Stock"; 
-        let inStockStyle = "productCard-Element productCard-productInStock productCard-productInStock-Out";
-        if(this.state.product.in_stock){
-            productCardClassName= "productCard productCardInStock";
-            inStockText = "In Stock";
-            inStockStyle = "productCard-Element productCard-productInStock productCard-productInStock-In";
-        }
-        return (<li className="productCardContainer">
-            <div className={productCardClassName}>
-                <div className="productCard-Element productCard-productName">{this.state.product.name}</div>
-                <div className={inStockStyle}>{inStockText}</div>
-                <div className="productCard-Element productCard-productPrice">${this.state.product.price}</div>
-            </div>
-        </li>);
-    }
-}
-*/
-
 
 
 const ProductCard = (props) => {
--- cantiin/src/routes/products/list.js
+++ cantiin/src/routes/products/list.js
@@ -6,99 +6,6 @@ import {getItemsList, getAllResultsNumber,
 import Pagination from '../../components/pagination';
 import ProductCard from '../../components/cards/product';
 
-
-
-/*
-class ProductsList extends Component {
-    state={
-        "loaded":false,
-        "items":[],
-        "response":null,
-        "success":false,
-        "err":null
-    }
-
-
-    /*Helpers */  /*
-
-    handleSucessfulResponse(response)
-    {
-        console.log(response);
-        
-        this.setState(
-        {
-            "loaded":true,
-            "items":getItemsList(response),
-            "response": response,
-            "success":true,
-            "err":null
-        });
-        console.log(this.state);
-    }
-
-    handleFailingResponse(err)
-    {
-        //"config", "request", "response", "isAxiosError", "toJSON"
-        /*console.log(err);
-        console.log(err.config);
-        console.log(err.response);
-        console.log(err.isAxiosError);
-        console.log(err.toJSON());
-        console.log(Object.keys(err));*/  /*
-        this.setState({
-            "loaded":true,
-            "items":[],
-            "response":null,
-            "success":false,
-            "err":err
-        }
-        );
-    }
-
-
-    /*Life Cycle*/  /*
-    componentDidMount()
-    {
-        productsListFetcher(getCurrentWindowPage())
-        .then((response)=>{this.handleSucessfulResponse(response);})
-        .catch((err)=>{this.handleFailingResponse(err);})
-    }
-
-
-
-
-
-    render() { 
-
-        if (!this.state.loaded)
-        {
-
-            return <div>
-            <h1>Products List:</h1>
-            <div>Loading...</div>
-        </div>;}
-        if(this.state.success)
-        {let productsList = this.state.items.map(product=>{return(
-            <Fragment>
-                <ProductCard item={product}/>
-            </Fragment>
-        );});
-        return <div>
-        <h1>Products List:</h1>
-        <Pagination response={this.state.response}/>
-        <ul className="ProductsList">
-            {productsList}
-        </ul>
-        </div>;}
-        return <div>
-        <h1>Products List:</h1>
-            <div>{this.state.err.response.data.detail}</div>
-            </div>;
-    }
-}
-*/
-
-
 const ProductsList = () => {
     const [state,setState] = useState({
         "loaded":false,
@@ -126,13 +33,6 @@ const ProductsList = () => {
     
     const handleFailingResponse = (err) =>
     {
-        //"config", "request", "response", "isAxiosError", "toJSON"
-        /*console.log(err);
-        console.log(err.config);
-        console.log(err.response);
-        console.log(err.isAxiosError);
-        console.log(err.toJSON());
-        console.log(Object.keys(err));*/
         setState({
             ...state,
             "loaded":true,
