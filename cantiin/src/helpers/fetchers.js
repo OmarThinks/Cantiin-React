d893e99779ca0736f5450393bc01274b6fee84cb
Omar Magdy <contact.parmag@gmail.com>
2021-10-02 02:10:02 +0200

    preparing fetchers

---
+++ cantiin/src/helpers/fetchers.js
@@ -0,0 +1,22 @@
+
+
+
+
+const fetchers={
+    products:
+    {
+        list:"list",
+        get:"get",
+        post:"post",
+        delete:"delete",
+        edit:"edit"
+    },
+    auth:
+    {
+        login:"login",
+        logout:"logout",
+        user:"user",
+    }
+};
+
+export default fetchers;
