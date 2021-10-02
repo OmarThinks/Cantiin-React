1b3dbc3edf7553e428cd3b8d987fe68871fd1108
Omar Magdy <contact.parmag@gmail.com>
2021-10-02 03:25:25 +0200

    error text

--- cantiin/src/components/forms/auth/login.js
+++ cantiin/src/components/forms/auth/login.js
@@ -19,7 +19,7 @@ const LoginForm = () => {
   const [form,setForm] = useState(
     {
       data:   {username:"", password:""},
-      errors: {username:"", password:""}
+      errors: {username:"abc", password:"defg"}
     });
 
 
@@ -59,10 +59,12 @@ const LoginForm = () => {
 <div>
     <label>Username: </label>
     <input type="text" name="username" value={form.data.username} onChange={handleChange}/>
+    <span className="form-error" name="username">{form.errors.username}</span>
     </div>
     <div>
     <label>Password: </label>
     <input type="text" name="password" value={form.data.password} onChange={handleChange}/>
+    <span className="form-error" name="password">{form.errors.password}</span>
     </div>
     <input type="submit" value="Login"/>
 
