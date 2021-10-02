f7f26136a4a3ab26fc67499050796687d3c154ea
Omar Magdy <contact.parmag@gmail.com>
2021-10-02 02:11:23 +0200

    productsListFetcher now moved to fetchers.js

--- cantiin/src/helpers/fetchers.js
+++ cantiin/src/helpers/fetchers.js
@@ -1,11 +1,23 @@
+import { settings } from '../settings';
+const axios = require('axios');
 
 
+function productsListFetcher(pageNumber = 1)
+{
+    var config = {
+        method: 'get',
+        url: settings.backend_urls.products.list+`?page=${pageNumber}`,
+          
+      };
+    let fetcher = axios(config);
+    return fetcher
+}
 
 
 const fetchers={
     products:
     {
-        list:"list",
+        list: productsListFetcher,
         get:"get",
         post:"post",
         delete:"delete",
